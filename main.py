9c65de2f4358215aabb0678bc87e517398b56b00
rzymski <83671265+rzymski@users.noreply.github.com>
2024-06-04 02:05:10 +0200

    zad6

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ ratingCollection = db['Rating']
 nameCollection = db['Name']
 
 
+def printDocList(docList):
+    for doc in docList:
+        print(doc)
+
+
 def zad1():
     titleCount = titleCollection.count_documents({})
     ratingCount = ratingCollection.count_documents({})
     nameCount = nameCollection.count_documents({})
-
-    print(f"Number of documents in 'Title' collection: {titleCount}")
-    print(f"Number of documents in 'Rating' collection: {ratingCount}")
-    print(f"Number of documents in 'Name' collection: {nameCount}\n")
+    ic(titleCount)
+    ic(ratingCount)
+    ic(nameCount)
 
 
 def zad2():
@@ -23,10 +27,9 @@ def zad2():
     zad2Display = {'_id': 0, 'primaryTitle': 1, 'startYear': 1, 'genres': 1, 'runtimeMinutes': 1}
     zad2Results = titleCollection.find(zad2Query, zad2Display).sort('primaryTitle', 1).limit(4)
     # Wyświetlenie 4 dokumentow
-    for doc in zad2Results:
-        print(doc)
-    totalCount = titleCollection.count_documents(zad2Query)
-    print(f"Total documents matching the criteria: {totalCount}\n")
+    printDocList(zad2Results)
+    zad2Count = titleCollection.count_documents(zad2Query)
+    ic(zad2Count)
 
 
 def zad3():
@@ -34,27 +37,49 @@ def zad3():
                  {'$group': {'_id': '$titleType', 'count': {'$sum': 1}}},
                  {'$project': {'_id': 0, 'titleType': '$_id', 'count': 1}}]
     zad3Result = titleCollection.aggregate(zad3Query)
-    for doc in zad3Result:
-        print(doc)
+    printDocList(zad3Result)
 
 
 def zad4a():
     zad4Query = {'genres': 'Documentary', 'startYear': {'$gte': 2010, '$lte': 2012}}
     zad4aResult = titleCollection.count_documents(zad4Query)
-    print(f"\nTotal documents matching the criteria: {zad4aResult}\n")
-
-# zad4bQuery = [{'$match': zad4Query},
-#               {'$lookup': {'from': 'Rating', 'localField': 'tconst', 'foreignField': 'tconst', 'as': 'joinRating'}},
-#               {'$unwind': '$joinRating'},
-#               {'$project': {'primaryTitle': 1, 'startYear': 1, 'averageRating': '$joinRating.averageRating'}},
-#               {'$sort': {'averageRating': -1}},
-#               {'$limit': 5}]
-# zad4bResult = list(titleCollection.aggregate(zad4bQuery))
-# for doc in zad4bResult:
-#     print(doc)
+    ic(zad4aResult)
 
 
-zad1()
-zad2()
-zad3()
-zad4a()
+def zad4b():
+    zad4bQuery = [{'$match': {'genres': 'Documentary', 'startYear': {'$gte': 2010, '$lte': 2012}}},
+                  {'$lookup': {'from': 'Rating', 'localField': 'tconst', 'foreignField': 'tconst', 'as': 'joinRating'}},
+                  {'$project': {'_id': 0, 'primaryTitle': 1, 'startYear': 1, 'averageRating': '$joinRating.averageRating'}},
+                  {'$sort': {'averageRating': -1}},
+                  {'$limit': 5}]
+    zad4bResult = list(titleCollection.aggregate(zad4bQuery))
+    printDocList(zad4bResult)
+
+
+def zad5():
+    # nameCollection.drop_index('primaryName_text')
+    nameCollection.create_index([('primaryName', 'text')])
+    zad5Query = {'$text': {'$search': 'Fonda Coppola', '$caseSensitive': True}}
+    zad5aResult = nameCollection.count_documents(zad5Query)
+    ic(zad5aResult)
+    zad5bResult = nameCollection.find(zad5Query, {'_id': 0, 'primaryName': 1, 'primaryProfession': 1}).limit(5)
+    printDocList(zad5bResult)
+
+
+def zad6():
+    nameCollection.create_index([('birthYear', -1)])
+    indexes = list(nameCollection.list_indexes())
+    for index in indexes:
+        ic(index)
+    indexCount = len(indexes)
+    ic(indexCount)
+
+# zad1()
+# zad2()
+# zad3()
+# zad4a()
+# zad4b()  # DZIALA, ALE WYKONUJE SIĘ PONAD 3 H
+# zad5() # COS POPSUTE
+zad6()
+
+
